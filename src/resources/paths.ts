27d67ecb2d02df050a69253b13eff250704f1c4e
idinium96 <47635037+idinium96@users.noreply.github.com>
2024-10-14 01:55:52 +0800

    🔨 fix wrong path

--- src/resources/paths.ts
+++ src/resources/paths.ts
@@ -17,11 +17,11 @@ export interface Paths {
 export default function genPaths(): Paths {
     return {
         files: {
-            options: path.join(__dirname, `../../../files/options.json`)
+            options: path.join(__dirname, `../../files/options.json`)
         },
         logs: {
-            log: path.join(__dirname, `../../../logs/%DATE%.log`),
-            error: path.join(__dirname, `../../../logs/error.log`)
+            log: path.join(__dirname, `../../logs/%DATE%.log`),
+            error: path.join(__dirname, `../../logs/error.log`)
         }
     };
 }
